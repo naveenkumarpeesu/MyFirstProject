fd0c8b2e8bc38567de3cf219b3f4fe74b48b37f2
naveenkumarpeesu <naveenkumar.peesu@gmail.com>
2017-07-10 18:00:55 -0500

    Revert "Develope"

--- src/com/naveen/main/NumberAddition.java
+++ src/com/naveen/main/NumberAddition.java
@@ -3,8 +3,6 @@ package com.naveen.main;
 public class NumberAddition {
 
 	public static void main(String[] args) {
-		System.out.println("Naveen is here and check in code");
-		System.out.println("This is develope branch");
 		System.out.println("Naveen is here 1234");
 	}
 
